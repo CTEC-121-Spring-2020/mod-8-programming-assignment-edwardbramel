3c14c76cfe47ecb56dc58631ad484a8719c9e746
Eddy Bramel <e.lamanna-bramel@students.clark.edu>
2020-06-11 22:18:50 -0700

    confused

--- Prob-1/Prob-1.py
+++ Prob-1/Prob-1.py
@@ -3,3 +3,49 @@
 #     Prob-1.py
 
 # Your code below
+
+
+class Cars:
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+
+
+def testcar():
+    carlot = []
+    howmany = int(input("how many cars you have on the lot? "))
+
+    for i in range(howmany):
+        make = input("what is the make? ")
+        model = input("what is the model? ")
+        year = input("what year is it? ")
+        car = Cars(make, model, year)
+        carlot.append(car)
+        print("the make for the car is", make,
+              "and its a", model, "model, and its made in the year", year)
+
+
+testcar()
+# testing
+'''
+# this is what you want
+
+
+class Cars:
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+
+
+def carlot():
+    car1make = "ford"
+    car1model = "gt"
+    car1year = "2007"
+    print("the first car is,", car1make, "the model of it is,",
+          car1model, " and the year for car 1 is", car1year)
+
+
+carlot()
+'''
--- Prob-2/Prob-2.py
+++ Prob-2/Prob-2.py
@@ -3,3 +3,38 @@
 #     Prob-2.py
 
 # Your code below
+'''text
+Name            ID Number       Major           GPA
+Joe Bella       9933            Web Development 3.8
+Tucker Blank    3399            Nursing         3.0
+Gayle Ujifusa   1011            Baking          2.8
+Edna Anker      9875            Medical Office  3.0
+'''
+
+
+class student:
+    def __init__(self, name, ID, major, GPA):
+        self.name = name
+        self.ID = ID
+        self.major = major
+        self.GPA = GPA
+
+    def getID(self):
+        return self.ID
+
+    def SetID(self, ID):
+        self.ID = ID
+
+
+def main():
+    #Joe = student("Joe Bella", "9933", "Web Dev", "3.8")
+    # print(Joe)
+
+    Joe = student("Joe Bella", "9933")
+    print(Joe.name)
+
+    #Joe.SetID("joe", "9933")
+    #print("joes id is:", Joe.getID())
+
+
+main()
